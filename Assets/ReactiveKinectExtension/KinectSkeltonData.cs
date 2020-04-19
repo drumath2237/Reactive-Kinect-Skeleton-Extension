292865bc0e3aba7848087a8882cb8cde1ec49bf4
drumath <2237k.drumath@gmail.com>
2020-04-20 01:28:48 +0900

    :sparkles: KinectSkeltonData

---
+++ Assets/ReactiveKinectExtension/KinectSkeltonData.cs
@@ -0,0 +1,30 @@
+﻿using Kinect = Windows.Kinect;
+
+namespace ReactiveKinectExtension
+{
+    public struct KinectSkeltonData
+    {
+        public KinectJointPose spineBase;
+        public KinectJointPose spineMid;
+        public KinectJointPose neck;
+        public KinectJointPose head;
+        public KinectJointPose shoulderLeft;
+        public KinectJointPose elbowLeft;
+        public KinectJointPose wristLeft;
+        public KinectJointPose handLeft;
+        public KinectJointPose shoulderRight;
+        public KinectJointPose elbowRight;
+        public KinectJointPose wristRight;
+        public KinectJointPose handRight;
+        public KinectJointPose hipLeft;
+        public KinectJointPose kneeLeft;
+        public KinectJointPose ankleLeft;
+        public KinectJointPose footLeft;
+        public KinectJointPose hipRight;
+        public KinectJointPose kneeRight;
+        public KinectJointPose ankleRight;
+        public KinectJointPose footRight;
+        public KinectJointPose spineShoulder;
+
+    }
+}
